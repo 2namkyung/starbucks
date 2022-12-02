efe29f3534c7c52d7f6d89590cb3137916d0f948
2namkyung <iddbxk@daum.net>
2022-12-02 23:57:52 +0900

    title -> submenu

--- src/components/Header/SubMenu/SubMenuLink.tsx
+++ src/components/Header/SubMenu/SubMenuLink.tsx
@@ -2,17 +2,17 @@ import { Link } from 'react-router-dom';
 
 type SubMenuLinkProps = {
   to: string;
-  title: string;
+  subMenu: string;
 };
 
-export default function SubMenuLink({ to, title }: SubMenuLinkProps) {
+export default function SubMenuLink({ to, subMenu }: SubMenuLinkProps) {
   return (
     <li className="relative first:before:hidden before:w-[1px] before:h-[12px] before:bg-[#e5e5e5] before:absolute before:top-0 before:bottom-0 before:m-auto">
       <Link
         className="block text-[#656565] text-xs px-[11px] py-[16px] cursor-pointer hover:text-black"
         to={to}
       >
-        {title}
+        {subMenu}
       </Link>
     </li>
   );
